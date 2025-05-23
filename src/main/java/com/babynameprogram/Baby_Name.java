611bc3b4b7549117ac0dd9e892440a9e94288bda
Yanghaiii <nyeinsaynaing1432@gmail.com>
2025-05-23 13:17:03 +0800

    First Commit

---
+++ src/main/java/com/babynameprogram/Baby_Name.java
@@ -0,0 +1,46 @@
+package com.babynameprogram;
+
+public class Baby_Name {
+    //name, gender, year, rank, count
+    private String name, gender;
+    private int year, rank, count;
+
+    public Baby_Name(String name, String gender, int year, int rank, int count) {
+        this.name = name;
+        this.gender = gender;
+        this.year = year;
+        this.rank = rank;
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return "Baby_Name{" +
+                "name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", year=" + year +
+                ", rank=" + rank +
+                ", count=" + count +
+                '}';
+    }
+}
