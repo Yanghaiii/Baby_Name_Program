ced6e146fe61fc3ad4441d37b73ae6d6f592d6c2
Yanghaiii <nyeinsaynaing1432@gmail.com>
2025-06-11 09:33:04 +0630

    Report_Top10_Baby_Name_By_Year

--- src/main/java/com/babynameprogram/Delete_Baby_Name_By_Year.java
+++ src/main/java/com/babynameprogram/Delete_Baby_Name_By_Year.java
@@ -9,7 +9,7 @@ public class Delete_Baby_Name_By_Year {
     public Delete_Baby_Name_By_Year(LinkedList<Baby_Name> namelist) {
         this.namelist = namelist;
         System.out.println("Deleting A Baby Name By Year");
-        System.out.println("--------------------");
+        System.out.println("----------------------------");
     }
 
     public void delete_Name() {
